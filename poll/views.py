479c5a3e7a232cba32293f981788397c7cf15d34
sakib <shahrearalsakib16@gmail.com>
2023-12-23 13:46:44 +0600

    Table Added for Practice

--- poll/views.py
+++ poll/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
 def indexes(request):
-    return render(request,'index.html')
+    peoples = [
+        {'name': 'Sakib' , 'Age' : '18'},
+        {'name': 'Ahedul' , 'Age' : '17'},
+        {'name': 'Kabya' , 'Age' : '16'},
+        {'name': 'Apurbo' , 'Age' : '28'},
+        {'name': 'Aziz' , 'Age' : '22'}
+    ]
+    
+    return render(request,'index.html', context = {'peoples' : peoples})
